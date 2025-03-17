105e6c85029b6616747f6b95093f84d75851b8a2
Amith_Sandesh <sandeshamith73@gmail.com>
2025-03-17 16:34:53 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 import Favourites from "./pages/Favourites";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { MovieProvider } from "./context/MovieContext";
+
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main>
         <Routes>
@@ -15,7 +17,7 @@ function App() {
           <Route path="/favourites" element={<Favourites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
